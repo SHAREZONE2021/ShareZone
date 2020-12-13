478c05289d6d3137da0e5aaf30d956b515734df5
Arindam8282 <darindam507@gmail.com>
2020-12-13 09:48:15 +0530

    test add

---
+++ precache-manifest.788236a72462609aea584383b15992b3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eef33545b317333d5b2",
+    "url": "/css/app.4c5a34e1.css"
+  },
+  {
+    "revision": "f711b30cc738a2e4dc31",
+    "url": "/css/chunk-1e251917.5fea66ed.css"
+  },
+  {
+    "revision": "03ac861351127991556a",
+    "url": "/css/chunk-59b6727a.0e433876.css"
+  },
+  {
+    "revision": "f6eaa4ba7ca32e1bb00b",
+    "url": "/css/chunk-7c85ada6.ecaf606e.css"
+  },
+  {
+    "revision": "e3d7c82b123fc777f4a08474d288b04e",
+    "url": "/img/background.jpg"
+  },
+  {
+    "revision": "175ab0f092c16ecb5a0c3724a59bc98c",
+    "url": "/img/logo-black.png"
+  },
+  {
+    "revision": "394c15c1b444c4468b2c7b0a6418c512",
+    "url": "/img/logo.394c15c1.png"
+  },
+  {
+    "revision": "394c15c1b444c4468b2c7b0a6418c512",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "733c0a062b8dd3a1401e2950be3cd49b",
+    "url": "/img/original.jpg"
+  },
+  {
+    "revision": "1152477830b464b30bc68d62097d1925",
+    "url": "/img/rose.11524778.jpg"
+  },
+  {
+    "revision": "1152477830b464b30bc68d62097d1925",
+    "url": "/img/rose.jpg"
+  },
+  {
+    "revision": "c7a0742d2c1566c16e7c73c4e60826e7",
+    "url": "/img/search.png"
+  },
+  {
+    "revision": "3ea077734388d2a3d6d044d065c4414c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eef33545b317333d5b2",
+    "url": "/js/app.5ba0d6e0.js"
+  },
+  {
+    "revision": "f711b30cc738a2e4dc31",
+    "url": "/js/chunk-1e251917.1ebb3ce1.js"
+  },
+  {
+    "revision": "03ac861351127991556a",
+    "url": "/js/chunk-59b6727a.7677f91f.js"
+  },
+  {
+    "revision": "f6eaa4ba7ca32e1bb00b",
+    "url": "/js/chunk-7c85ada6.f97c5d12.js"
+  },
+  {
+    "revision": "8f6bc47826afa0dc49a7",
+    "url": "/js/chunk-vendors.7c409d1d.js"
+  },
+  {
+    "revision": "071180f732cb55da4c89482399b900ad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ca7ce01aa6f3aace0769ea0231a9784e",
+    "url": "/style.css"
+  }
+]);
